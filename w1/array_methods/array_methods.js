a092c31bba8e7e82c33b573961fd8ecde1aa4ea1
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-23 15:59:30 +0300

    exe 12, 25

--- w1/array_methods/array_methods.js
+++ w1/array_methods/array_methods.js
@@ -13,8 +13,15 @@ const ExpensiveTipp = prices.filter(p => p > 100).map(p => p += p * 0.1)
 // 10
 const arr = [10, 20, 30];
 const mult = arr.reduce((acc, curr) => acc * curr, 1)
-console.log(mult);
-console.log(10*20*30);
 
+// 12
+const users = [
+    {name: "a", age: 15},
+    {name: "b", age: 25},
+    {name: "c", age: 35}
+]
+const av = users.reduce((acc, user) => acc + user.age, 0) / users.length
 
-
+// 25
+const nums1 = [1, 2, 4, 3, 5, ]
+const sortNums = [...nums1].sort((l, h) => h - l)
