96aedf1006a07cfbb746291bc2b6b1a69e0b9451
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-23 15:22:57 +0300

    exe 10

--- w1/array_methods/array_methods.js
+++ w1/array_methods/array_methods.js
@@ -4,10 +4,17 @@ const upper = words.map(w => w.toUpperCase())
 
 // 4
 const nums = [1, 2, 3, 4, 5, 6]
-const NewNums = nums.filter(n => n % 2 === 0)
+const newNums = nums.filter(n => n % 2 === 0)
 
 // 8
 const prices = [90, 100, 110]
 const ExpensiveTipp = prices.filter(p => p > 100).map(p => p += p * 0.1)
+
+// 10
+const arr = [10, 20, 30];
+const mult = arr.reduce((acc, curr) => acc * curr, 1)
+console.log(mult);
+console.log(10*20*30);
+
 
 
