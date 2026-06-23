a8e1eea41a716fc7a06c3421a424fe4db1566061
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-23 12:34:03 +0300

    exe 2, 4

---
+++ w1/array_methods/array_methods.js
@@ -0,0 +1,10 @@
+// 2
+const words = ["hello", "world"];
+const upper = words.map(w => w.toUpperCase())
+
+// 4
+const nums = [1, 2, 3, 4, 5, 6]
+const NewNums = nums.filter(n => n % 2 === 0)
+console.log(NewNums);
+
+
