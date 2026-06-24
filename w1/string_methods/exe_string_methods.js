d9bf01adb58c26a4514e8f7100e2d6db637d9f8c
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-24 15:23:59 +0300

    string methods

---
+++ w1/string_methods/exe_string_methods.js
@@ -0,0 +1,74 @@
+// 1
+console.log(" hello world ".trim());
+  
+
+// 2
+console.log("user@gmail.com".includes("@"));
+
+// 3
+console.log("javascript".toUpperCase());
+
+// 4
+console.log("HELLO WORLD".toLowerCase());
+
+// 6
+console.log("JavaScript".slice(0, 4));
+
+// 7
+console.log("JavaScript".slice(-6));
+
+// 8
+console.log("https://example.com".startsWith("http"));
+
+// 9
+console.log("mywebsite.org".endsWith(".com"));
+
+// 10
+console.log("שלום עולם".replace("שלום", "היי"));
+
+// 11
+console.log("banana".replaceAll("a", "o"));
+
+// 12
+console.log("one two three".split(" "));
+
+// 13
+console.log("apple,banana,grape".split(",").slice(0, 2));
+
+// 14
+console.log("banana".indexOf("a"));
+
+// 15
+console.log("banana".lastIndexOf("a"));
+
+// 16
+console.log("7".padStart(3, 0));
+
+// 18
+console.log("ha".repeat(3));
+
+// 19
+console.log("Hello".charAt(0));
+
+// 20
+console.log("Hello".at(-1));
+
+// 21
+console.log("שלום".concat(" ", "עולם"));
+
+// 22
+console.log("JavaScript".substring(2, 6));
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
