cff28769c0cec128c2b902ab17848b6cb79daa36
Yonatan Eiferman <yonieifer@gmail.com>
2026-06-28 12:11:27 +0300

    gitBasic

---
+++ w2/gitBasic/gitBasic.js
@@ -0,0 +1,58 @@
+/*
+-1-
+1
+תיקייה במעקב שמאפשרת ניהול גרסאות
+
+2
+git init
+
+3
+.git
+
+-2-
+1
+WD, STAGED, COMMITED
+
+2
+git add about.html
+git commit -m "added about.html"
+
+3
+מוכן לקומיט לבין עדיין לא או לא מוכר 
+
+-3-
+1
+git status
+
+2
+git log --oneline
+
+3
+git diff a1b2c3d e4f5g6h
+
+4
+סטטוס מראה סטטוס נוכחי של הקבצים לוג מראה את היסטוריית השינויים
+
+-4-
+1, 2
+node_modules
+.env
+
+3
+סיסמאות טוקנים ומפתחות בענן פתוח לכולם
+
+-5-
+git init
+git status
+touch .gitignore
+echo "__pycache__ " > .gitignore
+git add app.py
+git commit -m "added app.py"
+git diff 
+git add app.py 
+git commit -m "added func"
+
+
+
+
+*/
